16bfba4e39dcc9554ee2263909a293e9f1ff8c17
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-09-28 18:45:30 -0600

    feat: adds project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Ricardo Veronica Duran Web Developer</h1>
+
+      <h2>Design and Development Freelance</h2>
+
+      <p>Guadalajara, Jalisco</p>
+
+      <h2>My Services</h2>
+
+      <h3>Laravel</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, sapiente.
       </p>
+
+      <h3>React</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, eum repellendus!
+      </p>
+
+      <h3>Wordpress</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis!
+      </p>
+
+      <h2>Contact</h2>
+
+      <p>All Rights Reserved. Ricardo Veronica Duran</p>
     </>
   )
 }
