2fac387f9dc4544b8c50938b95136c87a698bdef
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-09-29 12:40:10 -0600

    feat: adds hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,38 +1,24 @@
+import Icon from "./Icon"
+
 import "./hero.css"
 
 const Hero = () => {
   return (
-    <>
-      <section>
+    <section className="hero">
+      <div className="hero__content">
         <h2>Design and Development Freelance</h2>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-map-2"
-          width="80"
-          height="80"
-          viewBox="0 0 24 24"
-          stroke-width="1"
-          stroke="#ffec00"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path d="M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7.5"></path>
-          <path d="M9 4v13"></path>
-          <path d="M15 7v5.5"></path>
-          <path
-            d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z"
-          ></path>
-          <path d="M19 18v.01"></path>
-        </svg>
+        <div className="hero__icontext">
+          <Icon />
 
-        <p>Guadalajara, Jalisco</p>
+          <p>
+            Guadalajara, Jalisco
+          </p>
+        </div>
 
-        <a href="#">Contact</a>
-      </section>
-    </>
+        <a className="button" href="#">Contact</a>
+      </div>
+    </section >
   )
 }
 
