65657a69af5deeaf01a7f3739bb192ed103947cb
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-09-29 09:09:26 -0600

    feat: adds nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,14 +2,14 @@ import "./nav.css"
 
 const Nav = () => {
   return (
-    <>
-      <nav>
+    <div className="main-nav__container">
+      <nav className="container main-nav">
         <a href="#">Home</a>
         <a href="#">Services</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
       </nav>
-    </>
+    </div>
   )
 }
 
